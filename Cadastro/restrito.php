fc8e8187d3be37a8e3411b8ef3f8ca67e448eb43
roninls <roninls@sempreceub.com>
2021-04-27 03:48:39 -0300

    FIX Cadastro deleted

--- Cadastro/restrito.php
+++
@@ -1,7 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['id'], $_SESSION['nome'], $_SESSION['email']);
-
-    $_SESSION['msg'] = "Área Restrita";
-    header("Location: ../login.php");
-?>
