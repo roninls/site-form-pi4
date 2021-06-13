f5a4df14e5a394d438bc723829000040d20e5a13
roninls <roninls@sempreceub.com>
2021-06-13 20:55:24 -0300

    updates

--- pags/teste/index.php
+++ pags/teste/index.php
@@ -33,11 +33,11 @@
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
+    
     <link href="css/style.css" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/controller.js"></script>
